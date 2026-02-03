c8a9056161976b6af64d8181f3da5da6e94868d0
leejaewook <leee913@naver.com>
2026-02-03 18:17:23 +0900

    2일차

--- src/main/java/com/ex01/basic/controller/MemberController.java
+++ src/main/java/com/ex01/basic/controller/MemberController.java
@@ -4,12 +4,15 @@ import com.ex01.basic.dto.MemberDto;
 import com.ex01.basic.exception.MemberDuplicateException;
 import com.ex01.basic.exception.MemberNotFoundException;
 import com.ex01.basic.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "MemberAPI", description = "회원 도메인 API")
 @RestController
 @RequestMapping("/members")
 public class MemberController {
@@ -29,6 +32,9 @@ public class MemberController {
         return ResponseEntity.ok("성공");
     }
     @GetMapping
+    @Operation(
+            summary = "전체 회원 조회"
+    )
     public ResponseEntity<List<MemberDto>> getList(){
         List<MemberDto>list = null;
         try {
@@ -39,6 +45,9 @@ public class MemberController {
         return ResponseEntity.ok(list);
     }
     @GetMapping("/{id}") // /members/{id}
+    @Operation(
+            summary = "특정 회원 조회"
+    )
     public ResponseEntity<MemberDto> getOne(@PathVariable("id") int id){
         MemberDto memberDto = null;
         //System.out.println("연결 확인 : "+id);
@@ -51,6 +60,9 @@ public class MemberController {
         return ResponseEntity.ok(memberDto);
     }
     @PutMapping("/{id}")
+    @Operation(
+            summary = "회원 수정"
+    )
     public ResponseEntity<Void> update(@ModelAttribute MemberDto memberDto,
                                         @PathVariable("id") int id){
         //System.out.println("연결 확인 : "+id);
@@ -63,6 +75,9 @@ public class MemberController {
         return ResponseEntity.ok().build();
     }
     @DeleteMapping("{id}")
+    @Operation(
+            summary = "회원 삭제"
+    )
     public ResponseEntity<Void> deleteMember(@PathVariable("id") int id){
         try {
             memberService.delMember( id );
@@ -73,6 +88,9 @@ public class MemberController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "회원 추가"
+    )
     public ResponseEntity<String> register(@ModelAttribute MemberDto memberDto){
         try {
             memberService.insert(memberDto);
